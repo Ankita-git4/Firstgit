31308e7fec9bfec2cb801a939681ea5acf482d5c
Ankita-git4 <85279621+Ankita-git4@users.noreply.github.com>
2021-06-03 14:38:14 +0530

    initial_of_math

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add implementation
+def add(x,y):
+  return x+y
+
+#Subtract implementation
+def subtract(x,y):
+  return x-y
+
+#Multiply implementation
+def multiply(x,y):
+  pass
+
+#Division implementation
+def divide(x,y):
+  if y==o:
+    return error
+  else
+    return x/y
+
+def square(c)
+  pass
